7e117c5f2fe4b4a9d75ee311d2fceff9f14ddb20
decige <ccy498@galim.com>
2026-06-23 21:07:48 +0800

    生产环境改为/api

--- src/config/index.js
+++ src/config/index.js
@@ -5,19 +5,21 @@ const EnvConfig={
         baseApi:"/api",
         mockApi:"https://apifoxmock.com/m1/4068509-0-default/api/home"
     },
-
+    
     test:{
         baseApi:"//test.future.com/api",
         mockApi:"https://apifoxmock.com/m1/4068509-0-default/api"
     },
     prod:{
-        baseApi:"//future.com/api",
-        mockApi:"https://apifoxmock.com/m1/4068509-0-default/api"
+        // baseApi:"//future.com/api",
+        // mockApi:"https://apifoxmock.com/m1/4068509-0-default/api"
+        baseApi:"/api",
+        mockApi:"/api"
     },
 }
 
 export default {
     ...EnvConfig[env],
     env,
-    mock:false,//为true,就会使用mockApi这个地址,如果关闭了mock,就会使用baseApi这个地址
+    mock:true,//为true,就会使用mockApi这个地址,如果关闭了mock,就会使用baseApi这个地址
 }
